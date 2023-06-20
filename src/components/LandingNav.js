bfcbfb670a0969f5f3a2d737be7366f36e531092
jr-cast <castanos@protonmail.com>
2023-06-20 13:21:41 -0600

    Add Links to landingpage and workouts views in navs

--- src/components/LandingNav.js
+++ src/components/LandingNav.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const LandingNav = () => (
   <nav className="flex h-[7em] justify-between items-center m-[0_auto] max-w-7xl">
-    <h1 className="font-[Newsreader] text-5xl text-blue-600 antialiased">
-      FitSmart
-    </h1>
+    <Link to="/">
+      <h1 className="font-[Newsreader] text-5xl text-blue-600 antialiased cursor-pointer">
+        FitSmart
+      </h1>
+    </Link>
     <ul className="w-1/3 flex justify-between items-center">
       <li className="cursor-pointer text-slate-800 antialiased">About Us</li>
       <li className="cursor-pointer text-slate-800 antialiased">Pricing</li>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -22,12 +23,16 @@ const Nav = () => {
   return (
     <header className={`sticky top-0 z-40 bg-white ${isScrolled && "opacity-95"}`}>
       <nav className="flex h-[7em] justify-between items-center m-[0_auto] max-w-7xl">
-        <h1 className="font-[Newsreader] text-5xl text-blue-600 antialiased">
-          FitSmart
-        </h1>
+        <Link to="/">
+          <h1 className="font-[Newsreader] text-5xl text-blue-600 antialiased cursor-pointer">
+            FitSmart
+          </h1>
+        </Link>
         <ul className="w-1/3 flex justify-between items-center">
           <li className="cursor-pointer">Profile</li>
-          <li className="cursor-pointer">Workouts</li>
+          <Link to="/exercise-plans">
+            <li className="cursor-pointer">Workouts</li>
+          </Link>
           <li className="cursor-pointer">Meal Plans</li>
           <button
             className="font-[Inter] border border-blue-600 py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800"
