82d628ec2a6f0732ea8d4ecacaf1fc77298acc9b
Shadman Ali <shadman.ali@live.co.uk>
2023-07-02 01:15:32 +0530

    Add Swagger schema and documentation for routes /get-all-goals and /get-all-compositions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,27 +1,80 @@
-import express from 'express';
-import config from '../config/config.js';
-import { v4 as uuidv4 } from 'uuid';
+import express from "express";
+import config from "../config/config.js";
+import { v4 as uuidv4 } from "uuid";
 import OpenAiProvider from "../providers/provider.js";
 import OpenAiService from "../services/service.js";
 import OpenAiController from "../controllers/controller.js";
 import FormModel from "../models/workoutForm.js";
-import bodyCompositionModel from '../models/bodyComposition.js';
-import goalsModel from '../models/goals.js';
+import bodyCompositionModel from "../models/bodyComposition.js";
+import goalsModel from "../models/goals.js";
 
 const router = express.Router();
-const provider = new OpenAiProvider(
-  config.llm.model,
-  config.llm.apikey,
-);
+const provider = new OpenAiProvider(config.llm.model, config.llm.apikey);
 const service = new OpenAiService(provider);
 const controller = new OpenAiController(service);
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Composition:
+ *       type: object
+ *       required:
+ *         - id
+ *         - age
+ *         - weight
+ *         - height
+ *         - bodyFat
+ *         - bodytype
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The composition's ID.
+ *         age:
+ *           type: integer
+ *           minimum: 0
+ *           description: The user's age.
+ *         weight:
+ *           type: integer
+ *           description: The user's weigth.
+ *         height:
+ *           type: integer
+ *           description: The user's height.
+ *         bodyFat:
+ *           type: integer
+ *           description: The user's body fat.
+ *         lbm:
+ *           type: integer
+ *           description: The user's lean body mass.
+ *         bmi:
+ *           type: integer
+ *           description: The user's body mass index.
+ *         waist:
+ *           type: integer
+ *           description: The user's waist.
+ *         bodytype:
+ *           type: string
+ *           description: The user's body type.
+ *       example:
+ *         id: 123
+ *         age: 30
+ *         weight: 70
+ *         height: 180
+ *         bodyFat: 15
+ *         lbm: 60
+ *         bmi: 21
+ *         waist: 80
+ *         bodytype: Mesomorph
+ */
 
 /**
  * @swagger
  * /get-all-compositions:
  *   get:
  *     summary: Get all compositions
+ *     description: Retrieve a list of all compositions.
+ *     tags:
+ *       - Compositions
  *     responses:
  *       '200':
  *         description: Successful response
@@ -29,47 +82,126 @@ const controller = new OpenAiController(service);
  *           application/json:
  *             schema:
  *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Composition'
  */
 // GET body compositions
-router.get('/get-all-compositions', (req, res) => {
+router.get("/get-all-compositions", (req, res) => {
   const getItems = async () => {
     const Items = await bodyCompositionModel.find({});
     return Items;
   };
-  getItems().then((data) => {
-    res.status(200).json(data);
-  }).catch((err) => {
-    res.status(400).json({ message: err.message });
-  });
+  getItems()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
 });
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Goals:
+ *       type: object
+ *       required:
+ *         - id
+ *         - trainingFocus
+ *         - weight
+ *         - bodyFat
+ *         - frequency
+ *         - cardio
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The composition's ID.
+ *         trainingFocus:
+ *           type: string
+ *           description: The user's training focus.
+ *         weight:
+ *           type: integer
+ *           description: The user's weight.
+ *         bodyFat:
+ *           type: integer
+ *           description: The user's body fat.
+ *         frequency:
+ *           type: integer
+ *           description: The exercise frequency.
+ *         lbm:
+ *           type: integer
+ *           description: The user's lbm.
+ *         bmi:
+ *           type: integer
+ *           description: The user's bmi.
+ *         flexibility:
+ *           type: boolean
+ *           description: The user's flexibility.
+ *         cardio:
+ *           type: boolean
+ *           description: The user's cardio.
+ *       example:
+ *         id: 123
+ *         trainingFocus: Hypertrophy
+ *         weight: 70
+ *         bodyFat: 15
+ *         frequency: 3
+ *         lbm: 60
+ *         bmi: 21
+ *         flexibility: true
+ *         cardio: true
+ */
+
+/**
+ * @swagger
+ * /get-all-goals:
+ *   get:
+ *     summary: Get all compositions
+ *     description: Retrieve a list of all compositions.
+ *     tags:
+ *       - Goals
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Goals'
+ */
 // GET created goals
-router.get('/get-all-goals', (req, res) => {
+router.get("/get-all-goals", (req, res) => {
   const getItems = async () => {
     const Items = await goalsModel.find({});
     return Items;
   };
-  getItems().then((data) => {
-    res.status(200).json(data);
-  }).catch((err) => {
-    res.status(400).json(({ message: err.message }));
-  })
-})
+  getItems()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+});
 
 // Get created exercise plans
-router.get('/get-exercise-forms', (req, res) => {
+router.get("/get-exercise-forms", (req, res) => {
   const getItems = async () => {
     const Items = await FormModel.find({});
     return Items;
   };
-  getItems().then((data) => {
-    res.status(200).json(data);
-  }).catch((err) => {
-    res.status(400).json(({ message: err.message }));
-  });
+  getItems()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
 });
 
-router.post('/create-form', async (req, res) => {
+router.post("/create-form", async (req, res) => {
   const data = new FormModel({
     id: uuidv4(),
     planName: req.body.planName,
@@ -87,7 +219,7 @@ router.post('/create-form', async (req, res) => {
   }
 });
 
-router.post('/body-composition', async (req, res) => {
+router.post("/body-composition", async (req, res) => {
   const data = new bodyCompositionModel({
     id: uuidv4(),
     age: req.body.age,
@@ -108,7 +240,7 @@ router.post('/body-composition', async (req, res) => {
   }
 });
 
-router.post('/create-goals', async (req, res) => {
+router.post("/create-goals", async (req, res) => {
   const data = new goalsModel({
     id: uuidv4(),
     trainingFocus: req.body.trainingFocus,
@@ -130,27 +262,26 @@ router.post('/create-goals', async (req, res) => {
 });
 
 // openai test endpoint
-router.post('/generate-workout', controller.generateText);
-
+router.post("/generate-workout", controller.generateText);
 
 // GET ALL
-router.get('/getAll', (req, res) => {
-  res.json('Get ALL API');
+router.get("/getAll", (req, res) => {
+  res.json("Get ALL API");
 });
 
 // GET by ID
-router.get('/getOne/:id', (req, res) => {
+router.get("/getOne/:id", (req, res) => {
   res.send(req.params.id);
 });
 
 // UPDATE by ID
-router.patch('/update/:id', (req, res) => {
-  res.send('Update by ID API');
+router.patch("/update/:id", (req, res) => {
+  res.send("Update by ID API");
 });
 
 // DELETE by ID
-router.delete('/delete/:id', (req, res) => {
-  res.send('Delete by ID API');
+router.delete("/delete/:id", (req, res) => {
+  res.send("Delete by ID API");
 });
 
 export default router;
