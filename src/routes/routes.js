9fd3e93d57cb0adf323875896217647fb33d1881
Shadman Ali <shadman.ali@live.co.uk>
2023-07-02 05:18:27 +0530

    Add Swagger schema and documentation for routes /get-exercise-forms and /create-form

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -186,6 +186,132 @@ router.get("/get-all-goals", (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Form:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The form's ID.
+ *           unique: true
+ *           example: "123"
+ *         planName:
+ *           type: string
+ *           description: The plan name.
+ *           example: "Weight Loss"
+ *         objective:
+ *           type: string
+ *           description: The objective of the form.
+ *           example: "Lose weight and build muscle"
+ *         bodyComposition:
+ *           type: object
+ *           properties:
+ *             age:
+ *               type: number
+ *               description: The user's age.
+ *               example: 30
+ *             weight:
+ *               type: number
+ *               description: The user's weight.
+ *               example: 70
+ *             height:
+ *               type: number
+ *               description: The user's height.
+ *               example: 180
+ *             bodyFat:
+ *               type: number
+ *               description: The user's body fat percentage.
+ *               example: 15
+ *             leanBodyMass:
+ *               type: number
+ *               description: The user's lean body mass.
+ *               example: 60
+ *             waist:
+ *               type: number
+ *               description: The user's waist measurement.
+ *               example: 80
+ *             bodyType:
+ *               type: string
+ *               description: The user's body type.
+ *               example: "Mesomorph"
+ *           example:
+ *             age: 30
+ *             weight: 70
+ *             height: 180
+ *             bodyFat: 15
+ *             leanBodyMass: 60
+ *             waist: 80
+ *             bodyType: "Mesomorph"
+ *         fitnessGoal:
+ *           type: object
+ *           properties:
+ *             trainingFocus:
+ *               type: string
+ *               description: The user's training focus.
+ *               example: "Strength training"
+ *             weight:
+ *               type: number
+ *               description: The user's weight goal.
+ *               example: 65
+ *             bodyFat:
+ *               type: number
+ *               description: The user's body fat goal.
+ *               example: 12
+ *             frequency:
+ *               type: number
+ *               description: The exercise frequency per week.
+ *               example: 4
+ *             trainingType:
+ *               type: string
+ *               description: The type of training.
+ *               example: "Resistance training"
+ *             intensityLevel:
+ *               type: string
+ *               description: The intensity level of the training.
+ *               example: "Moderate"
+ *             mobility:
+ *               type: boolean
+ *               description: Indicates if mobility exercises are included.
+ *               example: true
+ *             cardio:
+ *               type: boolean
+ *               description: Indicates if cardio exercises are included.
+ *               example: true
+ *           example:
+ *             trainingFocus: "Strength training"
+ *             weight: 65
+ *             bodyFat: 12
+ *             frequency: 4
+ *             trainingType: "Resistance training"
+ *             intensityLevel: "Moderate"
+ *             mobility: true
+ *             cardio: true
+ *         status:
+ *           type: string
+ *           description: The status of the form.
+ *           example: "Pending"
+ */
+/**
+ * @swagger
+ * /get-exercise-forms:
+ *   get:
+ *     summary: Get all forms
+ *     description: Retrieve a list of all forms.
+ *     tags:
+ *       - Workout Form
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Form'
+ */
 // Get created exercise plans
 router.get("/get-exercise-forms", (req, res) => {
   const getItems = async () => {
@@ -201,6 +327,28 @@ router.get("/get-exercise-forms", (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /create-form:
+ *   post:
+ *     summary: Create form
+ *     description: Create form.
+ *     tags:
+ *       - Workout Form
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Form'
+ *     responses:
+ *       '200':
+ *         description: Form created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+*                 $ref: '#/components/schemas/Form'
+ */
 router.post("/create-form", async (req, res) => {
   const data = new FormModel({
     id: uuidv4(),
