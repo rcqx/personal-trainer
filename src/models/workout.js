7a7df7ba8a466ea8c3100ea4326f3da2bbcebe6d
jr-cast <castanos@protonmail.com>
2023-07-01 18:19:44 -0600

    fix: Update mongoose schema

--- src/models/workout.js
+++ src/models/workout.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const WorkoutSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   responseTest: {
     type: String,
     required: true,
