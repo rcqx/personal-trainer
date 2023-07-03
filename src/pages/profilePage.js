6248c9e7e662e2e037212e683a996d07dc628cc7
jr-cast <castanos@protonmail.com>
2023-07-02 21:59:51 -0600

    Add loaders and refresh profile data

--- src/pages/profilePage.js
+++ src/pages/profilePage.js
@@ -1,6 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import profilePicture from "../assets/profile.jpg";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const ProfilePage = () => {
+  const [compositions, setCompositions] = useState([]);
+  const [loader1, setLoader1] = useState(true);
+  const [loader2, setLoader2] = useState(true);
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL1 = "http://localhost:8080/api/get-all-compositions";
+      axios.get(URL1).then((res) => {
+        setCompositions(res.data)
+        setLoader1(false);
+      });
+
+      const URL2 = "http://localhost:8080/api/get-all-goals";
+      axios.get(URL2).then((res) => {
+        setGoals(res.data)
+        setLoader2(false);
+      });
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(goals);
+
   return (
     <div className="flex flex-col justify-between mt-10 mb-20">
       <div className="justify-between items-center m-[0_auto] max-w-7xl w-full">
@@ -20,50 +48,62 @@ const ProfilePage = () => {
           </div>
 
           <div className="border border-slate-100 antialiased flex-1 flex flex-col justify-center items-center rounded-xl shadow-lg">
-            <div className="w-5/6">
-              <h2 className="mb-4 text-xl font-semibold">Body Composition</h2>
-              <ul className="w-full">
-                <li className="flex justify-between"><h3 className="font-light">Age</h3>              <h3 className="font-semibold">33</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Weight (kg)</h3>      <h3 className="font-semibold">85 kg</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Height (cm)</h3>      <h3 className="font-semibold">180 cm</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Body Fat (%)</h3>     <h3 className="font-semibold">19%</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Lean Body Mass</h3>   <h3 className="font-semibold">60</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Body Mass Index</h3>  <h3 className="font-semibold">60</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Waist (cm)</h3>       <h3 className="font-semibold">81 cm</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Body Type</h3>        <h3 className="font-semibold">Endomorph</h3></li>
-              </ul>
-              <button
-                className="font-[Inter] border border-blue-600 py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800 mt-8 w-full"
-              >
-                Update
-              </button>
-            </div>
+
+            {!loader1 ? (
+              <div className="w-5/6 h-80 flex flex-col justify-between">
+                <h2 className="mb-4 text-xl font-semibold">Body Composition</h2>
+                <ul className="w-full">
+                  <li className="flex justify-between"><h3 className="font-light">Age</h3>              <h3 className="font-semibold">{compositions[0]?.age}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Weight (kg)</h3>      <h3 className="font-semibold">{compositions[0]?.weight} kg</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Height (cm)</h3>      <h3 className="font-semibold">{compositions[0]?.height} cm</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Body Fat (%)</h3>     <h3 className="font-semibold">{compositions[0]?.bodyFat}%</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Lean Body Mass</h3>   <h3 className="font-semibold">{compositions[0]?.lbm}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Body Mass Index</h3>  <h3 className="font-semibold">{compositions[0]?.bmi}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Waist (cm)</h3>       <h3 className="font-semibold">{compositions[0]?.waist} cm</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Body Type</h3>        <h3 className="font-semibold">{compositions[0]?.bodytype}</h3></li>
+                </ul>
+                <button
+                  className="font-[Inter] border border-blue-600 py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800 mt-8 w-full"
+                >
+                  Update
+                </button>
+              </div>
+
+            ) : (
+              <div className="w-5/6 h-80 flex flex-col justify-center items-center">
+                <BiLoaderAlt size={80} className="animate-spin" />
+              </div>
+            )}
           </div>
 
           <div className="border border-slate-100 antialiased flex-1 flex flex-col justify-center items-center rounded-xl shadow-lg">
-            <div className="w-5/6">
-              <h2 className="mb-4 text-xl font-semibold">Trainning Goals</h2>
-              <ul className="w-full">
-                <li className="flex justify-between"><h3 className="font-light">Trainning Focus</h3>        <h3 className="font-semibold">Hypertrophy</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Weight (kg)</h3>            <h3 className="font-semibold">78 kg</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Body Fat %</h3>             <h3 className="font-semibold">15 %</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Frequency</h3>              <h3 className="font-semibold">5 p/w</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Lean Body Mass</h3>         <h3 className="font-semibold">60</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Body Mass Index</h3>        <h3 className="font-semibold">25</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Flexibility / Mobility</h3> <h3 className="font-semibold">True</h3></li>
-                <li className="flex justify-between"><h3 className="font-light">Cardio</h3>                 <h3 className="font-semibold">True</h3></li>
-              </ul>
-              <button
-                className="font-[Inter] border border-blue-600 py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800 mt-8 w-full"
-              >
-                Update
-              </button>
-            </div>
+            {!loader1 ? (
+              <div className="w-5/6 h-80 flex flex-col justify-between">
+                <h2 className="mb-4 text-xl font-semibold">Trainning Goals</h2>
+                <ul className="w-full">
+                  <li className="flex justify-between"><h3 className="font-light">Trainning Focus</h3>        <h3 className="font-semibold">{goals[0]?.trainingFocus}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Weight (kg)</h3>            <h3 className="font-semibold">{goals[0]?.weight} kg</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Body Fat %</h3>             <h3 className="font-semibold">{goals[0]?.bodyFat} %</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Frequency</h3>              <h3 className="font-semibold">{goals[0]?.frequency} p/w</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Lean Body Mass</h3>         <h3 className="font-semibold">{goals[0]?.lbm}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Body Mass Index</h3>        <h3 className="font-semibold">{goals[0]?.bmi}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Flexibility / Mobility</h3> <h3 className="font-semibold">{goals[0]?.flexibility ? "True" : "False"}</h3></li>
+                  <li className="flex justify-between"><h3 className="font-light">Cardio</h3>                 <h3 className="font-semibold">{goals[0]?.cardio ? "True" : "False"}</h3></li>
+                </ul>
+                <button
+                  className="font-[Inter] border border-blue-600 py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800 mt-8 w-full"
+                >
+                  Update
+                </button>
+              </div>) : (
+              <div className="w-5/6 h-80 flex flex-col justify-center items-center">
+                <BiLoaderAlt size={80} className="animate-spin" />
+              </div>
+            )}
           </div>
-
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
