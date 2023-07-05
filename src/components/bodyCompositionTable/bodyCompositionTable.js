4fa6d958113398dc0bac5f0adcdba704b6a5d9a9
jr-cast <castanos@protonmail.com>
2023-07-04 22:47:15 -0600

    chore: update composition modal

--- src/components/bodyCompositionTable/bodyCompositionTable.js
+++ src/components/bodyCompositionTable/bodyCompositionTable.js
@@ -1,39 +1,63 @@
 const BodyCompositionTable = ({ compositions, setBcModal }) => {
   return (
     <div className="w-5/6 h-80 flex flex-col justify-between">
-      <h2 className="mb-4 text-xl font-semibold">Body Composition</h2>
+      <h2 className="mb-4 text-xl font-semibold antialiased text-slate-700">
+        Body Composition
+      </h2>
       <ul className="w-full">
         <li className="flex justify-between">
-          <h3 className="font-light">Age</h3>
-          <h3 className="font-semibold">{compositions[0]?.age}</h3>
+          <h3 className="font-light text-slate-600 antialiased">Age</h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.age}
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Weight (kg)</h3>
-          <h3 className="font-semibold">{compositions[0]?.weight} kg</h3>
+          <h3 className="font-light text-slate-600 antialiased">Weight (kg)</h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.weight} kg
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Height (cm)</h3>
-          <h3 className="font-semibold">{compositions[0]?.height} cm</h3>
+          <h3 className="font-light text-slate-600 antialiased">Height (cm)</h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.height} cm
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Body Fat (%)</h3>
-          <h3 className="font-semibold">{compositions[0]?.bodyFat}%</h3>
+          <h3 className="font-light text-slate-600 antialiased">
+            Body Fat (%)
+          </h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.bodyFat}%
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Lean Body Mass</h3>
-          <h3 className="font-semibold">{compositions[0]?.lbm}</h3>
+          <h3 className="font-light text-slate-600 antialiased">
+            Lean Body Mass
+          </h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.lbm}
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Body Mass Index</h3>
-          <h3 className="font-semibold">{compositions[0]?.bmi}</h3>
+          <h3 className="font-light text-slate-600 antialiased">
+            Body Mass Index
+          </h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.bmi}
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Waist (cm)</h3>
-          <h3 className="font-semibold">{compositions[0]?.waist} cm</h3>
+          <h3 className="font-light text-slate-600 antialiased">Waist (cm)</h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.waist} cm
+          </h3>
         </li>
         <li className="flex justify-between">
-          <h3 className="font-light">Body Type</h3>
-          <h3 className="font-semibold">{compositions[0]?.bodytype}</h3>
+          <h3 className="font-light text-slate-600 antialiased">Body Type</h3>
+          <h3 className="font-semibold antialiased text-slate-700">
+            {compositions[0]?.bodytype}
+          </h3>
         </li>
       </ul>
       <button
--- src/components/updateModal/updateModal.js
+++ src/components/updateModal/updateModal.js
@@ -1,19 +1,64 @@
 import { IoMdClose } from "react-icons/io";
+import { GrUpdate } from "react-icons/gr";
 
 const UpdateModal = ({ bcModal, setBcModal }) => {
   return (
-    <div className="border-2 border-black w-1/3 h-1/2 rounded-xl flex justify-center bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <form className="border border-red-600 w-full p-10">
+    <div className="border border-slate-200 shadow-lg w-1/3 h-2/3 rounded-xl flex justify-center bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <form className="w-full p-10 flex flex-col justify-between">
         <div
           className="absolute right-3 top-3 cursor-pointer"
           onClick={() => setBcModal(!bcModal)}
         >
           <IoMdClose size={35} />
         </div>
-        <div className="flex flex-col">
-          <label>Test</label>
-          <input type="text" className="border border-slate-800" />
+        <div className="flex flex-col items-start">
+          <label className="mb-1 text-slate-700 text-sm">Weight (kg)</label>
+          <input
+            type="number"
+            className="border border-slate-200 px-2 py-1 rounded-md focus:outline-none w-full"
+            placeholder="Weight in Kilograms"
+          />
         </div>
+        <div className="flex flex-col items-start">
+          <label className="mb-1 text-slate-700 text-sm">Body Fat (%)</label>
+          <input
+            type="number"
+            className="border border-slate-200 px-2 py-1 rounded-md focus:outline-none w-full"
+            placeholder="Body fat percentage"
+          />
+        </div>
+        <div className="flex flex-col items-start">
+          <label className="mb-1 text-slate-700 text-sm">
+            Lean Body Mass (LBM)
+          </label>
+          <input
+            type="number"
+            className="border border-slate-200 px-2 py-1 rounded-md focus:outline-none w-full"
+            placeholder="LBM units"
+          />
+        </div>
+        <div className="flex flex-col items-start">
+          <label className="mb-1 text-slate-700 text-sm">
+            Body Mass Index (BMI)
+          </label>
+          <input
+            type="number"
+            className="border border-slate-200 px-2 py-1 rounded-md focus:outline-none w-full"
+            placeholder="BMI units"
+          />
+        </div>
+        <div className="flex flex-col items-start">
+          <label className="mb-1 text-slate-700 text-sm">Waist (cm)</label>
+          <input
+            type="number"
+            className="border border-slate-200 px-2 py-1 rounded-md focus:outline-none w-full"
+            placeholder="Add your current waist measurement"
+          />
+        </div>
+        <button className="rounded-lg py-2 bg-blue-600 text-white flex justify-center items-center">
+          <GrUpdate size={15} className="mr-2" />
+          Update Body Composition
+        </button>
       </form>
     </div>
   );
