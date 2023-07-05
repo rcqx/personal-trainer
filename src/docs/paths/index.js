f8560b748ff5ee80dc1b50d04a6664478e12532a
Shadman Ali <shadman.ali@live.co.uk>
2023-07-05 19:22:18 +0530

    Add workout form paths

--- src/docs/paths/index.js
+++ src/docs/paths/index.js
@@ -1,11 +1,16 @@
 import { getBodyComposition, createBodyComposition } from "./bodyCompositionPaths.js";
 import { getGoals, createGoal } from "./goalsPaths.js";
+import { getWorkoutForm, createWorkoutForm } from "./workoutFormPaths.js";
+
+
 const paths = {
   paths: {
     ...getBodyComposition,
     ...createBodyComposition,
     ...getGoals,
-    ...createGoal
+    ...createGoal,
+    ...getWorkoutForm,
+    ...createWorkoutForm
   },
 };
 
---
+++ src/docs/paths/workoutFormPaths.js
@@ -0,0 +1,56 @@
+export const getWorkoutForm = {
+  "/get-exercise-forms": {
+    get: {
+      summary: "Get all forms",
+      description: "Retrieve a list of all forms.",
+      tags: ["Workout Form"],
+      responses: {
+        200: {
+          description: "Successful response",
+          content: {
+            "application/json": {
+              schema: {
+                type: "array",
+                items: {
+                  $ref: "#/components/schemas/Form",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export const createWorkoutForm = {
+  "/create-form": {
+    post: {
+      summary: "Create form",
+      description: "Create form.",
+      tags: ["Workout Form"],
+      requestBody: {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/WorkoutForm",
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: "Form created successfully",
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/WorkoutForm",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
