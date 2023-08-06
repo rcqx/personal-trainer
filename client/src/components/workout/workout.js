52e438ce5f29ca20700ff0ba34507dc776745cf8
jr-cast <castanos@protonmail.com>
2023-08-06 17:18:02 -0600

    chore: Remove meals from nav (temporaly) and manipulate workout string

--- client/src/components/workout/workout.js
+++ client/src/components/workout/workout.js
@@ -2,25 +2,41 @@ import { useState, useEffect } from "react";
 import { getWorkouts } from "../../api/workouts";
 
 const Workout = () => {
-  const [workouts, setWorkouts] = useState([]);
+  const [workout, setWorkout] = useState([]);
+  const [parsedWorkout, setParseWorkout] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await getWorkouts();
-        setWorkouts(response.data);
+        setWorkout(response.data[0].response);
       } catch (error) {
         console.log(error);
       }
     };
-
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (workout.length > 0) setParseWorkout(workout.split("Day ").slice(1));
+  }, [workout])
+
   return (
-    <div className="flex min-h-[40em] justify-between 
-    items-center m-[0_auto] max-w-7xl">
-      <h1>{workouts[0]?.response}</h1>
+    <div className="flex min-h-[40em] justify-center flex-col
+    items-center m-[0_auto] max-w-7xl border border-red-600 cursor-pointer">
+
+      {parsedWorkout.map((dayRoutine, index) => {
+        const [day, ...exercises] = dayRoutine.trim().split("\n");
+        return (
+          <div key={index} className="border border-red-600 w-2/3 min-h-40">
+            <p className="w-full text-start">Day {day}</p>
+            {exercises.map((exercise, exerciseIndex) => (
+              <p key={exerciseIndex} className="text-start">{exercise}</p>
+            ))}
+          </div>
+        );
+      })}
+
     </div>
   );
 };
