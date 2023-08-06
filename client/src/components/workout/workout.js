417649fb3dcc71f32498c4bce7612c11c46df1e9
jr-cast <castanos@protonmail.com>
2023-08-06 17:56:19 -0600

    chore: Refactor and rerender workout string and add react icons to view

--- client/src/components/workout/workout.js
+++ client/src/components/workout/workout.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { getWorkouts } from "../../api/workouts";
+import { MdDownload } from "react-icons/md";
+import { BsTable } from "react-icons/bs";
 
 const Workout = () => {
   const [workout, setWorkout] = useState([]);
@@ -18,26 +20,35 @@ const Workout = () => {
   }, []);
 
   useEffect(() => {
-    if (workout.length > 0) setParseWorkout(workout.split("Day ").slice(1));
+    if (workout.length > 0) {
+      setParseWorkout(workout.split("Day ").slice(1))
+    };
   }, [workout])
 
   return (
-    <div className="flex min-h-[40em] justify-center flex-col
-    items-center m-[0_auto] max-w-7xl border border-red-600 cursor-pointer">
+    <div className="flex min-h-[40em] justify-center flex-col gap-3 py-20 relative
+    items-center m-[0_auto] max-w-7xl border-4 border-dashed border-slate-200">
+      <div className="text-slate-400 absolute right-2 top-3 cursor-pointer flex items-center gap-1">
+        <BsTable size={30} />
+        <MdDownload size={40} />
+      </div>
 
-      {parsedWorkout.map((dayRoutine, index) => {
-        const [day, ...exercises] = dayRoutine.trim().split("\n");
-        return (
-          <div key={index} className="border border-red-600 w-2/3 min-h-40">
-            <p className="w-full text-start">Day {day}</p>
-            {exercises.map((exercise, exerciseIndex) => (
-              <p key={exerciseIndex} className="text-start">{exercise}</p>
-            ))}
-          </div>
-        );
-      })}
-
-    </div>
+      {
+        parsedWorkout.map((dayRoutine, index) => {
+          const [day, ...exercises] = dayRoutine.trim().split("\n");
+          return (
+            <div key={index} className="border-2 border-blue-300 w-[70%] min-h-40 p-8 shadow-md">
+              <h1 className="w-full text-start pb-2 text-lg text-slate-800 font-semibold">Day {day}</h1>
+              <ul className="pl-6">
+                {exercises.map((exercise, exerciseIndex) => (
+                  <li key={exerciseIndex} className="text-start text-slate-700 pb-1">{exercise}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })
+      }
+    </div >
   );
 };
 
