6aab42d37b75c29c960a70a13f2e05543fcf864f
Shadman Ali <shadman.ali@live.co.uk>
2023-08-09 03:21:44 +0530

    feat: add route to get fitness goal by id

--- server/src/routes/v1/goal.js
+++ server/src/routes/v1/goal.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 import {
-    getGoals, addGoal, updateGoal, deleteFitnessGoal
+    getGoals, addGoal, updateGoal, deleteFitnessGoal, getFitnessGoalById
 } from "../../controllers/goal.js";
 
 const router = express.Router();
 
 // GET goals
 router.get("/", getGoals);
+router.get("/:_id", getFitnessGoalById);
 // create goal
 router.post("/new", addGoal);
 router.put("/update", updateGoal);
