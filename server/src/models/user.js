e40a250c157e9db27d114f3c15bdf19428095112
Shadman Ali <shadman.ali@live.co.uk>
2023-08-16 05:14:43 +0530

    feat: encrypt password, check confirm password

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  _id: {
+  id: {
     type: String,
     unique: true, // Ensure the ID is unique
   },
@@ -55,6 +56,27 @@ const userSchema = new mongoose.Schema({
     type: Date,
   },
 });
+
+// Add a pre-save middleware to hash the password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next(); // Skip hashing if password isn't modified
+  }
+
+  try {
+    const hashedPassword = await hashPassword(this.password);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Hash the password using your preferred hashing library (e.g., bcrypt)
+async function hashPassword(password) {
+  const saltRounds = 10; // Number of salt rounds for bcrypt
+  return await bcrypt.hash(password, saltRounds);
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
